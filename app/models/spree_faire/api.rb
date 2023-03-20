e1c97211be469129676ec6592959c14496d88fc0
Michael Davidson <mdavo6@gmail.com>
2023-03-20 17:27:55 +1100

    Change to pull orders which are not due for shipment

--- app/models/spree_faire/api.rb
+++ app/models/spree_faire/api.rb
@@ -8,8 +8,9 @@ class SpreeFaire::Api
   
   def get_processing_orders
     excluded_states = "NEW,PRE_TRANSIT,IN_TRANSIT,DELIVERED,PENDING_RETAILER_CONFIRMATION,BACKORDERED,CANCELED"
-    ship_after_max = Time.now.utc.iso8601
-    @response = SpreeFaire::Request.new(@store).get("/orders?excluded_states=" + excluded_states + "&ship_after_max=" + ship_after_max)
+    # Can include ship_after_max to only pull orders which are eligible for shipping (ie. exclude orders with a future shipment date)
+    # ship_after_max = Time.now.utc.iso8601
+    @response = SpreeFaire::Request.new(@store).get("/orders?excluded_states=" + excluded_states) # + "&ship_after_max=" + ship_after_max)
   end
   
   def products
